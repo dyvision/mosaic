a44ce5f017a5522e427beb39dc4688a8a40cfe15
dyvision <dlang@dssinc.com>
2021-02-12 12:31:42 -0500

    woops

--- alltop.php
+++ alltop.php
@@ -1,5 +1,5 @@
 <?php
- //ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+//ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 
 
 include('lib/mosaic.php');
@@ -19,13 +19,13 @@ echo "<head><link href='style/style.css' rel='stylesheet'></head>";
 
 $users = json_decode($usercli->get(), true);
 foreach ($users as $user) {
-    $token = json_decode($authcli->authenticate($user,'refresh_token'),true);
-    $username = json_decode($authcli->verify($token['access_token']),true);
+    $token = json_decode($authcli->authenticate($user, 'refresh_token'), true);
+    $username = json_decode($authcli->verify($token['access_token']), true);
 
-    echo "<div class='block'><h2>".$username['display_name']."</h2>";
-    foreach(json_decode($playlistcli->get($token['access_token']),true) as $track){
+    echo "<div class='block'><h2>" . $username['display_name'] . "</h2>";
+    foreach (json_decode($playlistcli->get($token['access_token']), true) as $track) {
         echo "<span>$track</span></br>";
     }
 
+    echo "</div>";
 }
-echo "</div>";
