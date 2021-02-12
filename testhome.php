2a2938dd5b3310610f47ab2f8171fd4598baa867
dyvision <dlang@dssinc.com>
2021-02-12 15:39:15 -0500

    woops4

--- testhome.php
+++ testhome.php
@@ -28,7 +28,7 @@ foreach ($users as $user) {
         $ava = $username['images'][0]['url'];
     }
 
-    echo "<div class='block'><img class='profile' src='".$ava."'></img><a href='".$username['external_urls']['spotify']."'><h2>" . $username['display_name'] . "</h2></a>";
+    echo "<div class='block'><img src='".$ava."'></img><a href='".$username['external_urls']['spotify']."'><h2>" . $username['display_name'] . "</h2></a>";
     foreach (json_decode($playlistcli->get($token['access_token']), true) as $track) {
         echo "<a href='".$track['link']."'>".$track['name']."</a></br>";
     }
