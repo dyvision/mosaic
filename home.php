be287c7c62f76c37203f999dc6fbafe1d2cf1c42
dyvision <dlang@dssinc.com>
2021-02-12 13:07:43 -0500

    br

--- home.php
+++ home.php
@@ -15,7 +15,7 @@ $playlistcli = new top();
 $array = array();
 
 
-echo "<head><title>Mosaic</title><link href='style/style.css' rel='stylesheet'></head><body><a href='authorize.php'>Share your top tracks for the month</a>";
+echo "<head><title>Mosaic</title><link href='style/style.css' rel='stylesheet'></head><body><a href='authorize.php'>Share your top tracks for the month</a></br>";
 
 $users = json_decode($usercli->get(), true);
 foreach ($users as $user) {
