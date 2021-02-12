55a2947269b341ee1d0eb1266d23b6f0eff610d5
dyvision <dlang@dssinc.com>
2021-02-12 16:00:02 -0500

    new home css

--- home.php
+++ home.php
@@ -16,18 +16,23 @@ $array = array();
 
 
 echo "<head><title>Mosaic</title><meta name='viewport' content='width=device-width, initial-scale=1'>
-<link href='style/style.css' rel='stylesheet'></head><body><a href='authorize.php'>Share your top tracks for the month</a></br><center><h1>Welcome to Mosaic</h1> <h3>Check out everyone's top songs for the past 4 weeks. Click on a song to listen to it or share yours by clicking the link in the top left</h3></center></br>";
+<link href='style/style.css' rel='stylesheet'></head><body><a href='authorize.php'>Share your top tracks for the month</a></br><center><h1>Welcome to Mosaic</h1> <h3>Check out everyone's top songs for the past 4 weeks. Click on a song to listen to it or share yours by clicking the link in the top left</h3></center></br><center>";
 
 $users = json_decode($usercli->get(), true);
 foreach ($users as $user) {
     $token = json_decode($authcli->authenticate($user, 'refresh_token'), true);
     $username = json_decode($authcli->verify($token['access_token']), true);
-
-    echo "<div class='block'><a href='".$username['external_urls']['spotify']."'><h2>" . $username['display_name'] . "</h2></a>";
-    foreach (json_decode($playlistcli->get($token['access_token']), true) as $track) {
-        echo "<a href='".$track['link']."'>".$track['name']."</a></br>";
+    if($username['images'][0]['url'] == null){
+        $ava = 'https://www.tenforums.com/geek/gars/images/2/types/thumb_15951118880user.png';
+    } else{
+        $ava = $username['images'][0]['url'];
     }
 
-    echo "</div>";
+    echo "<div class='block'><img class='profile' src='".$ava."'></img><div class='songlist'><a href='".$username['external_urls']['spotify']."'><h2>" . $username['display_name'] . "</h2></a>";
+    foreach (json_decode($playlistcli->get($token['access_token']), true) as $track) {
+        echo "<a href='".$track['link']."'>".$track['name']."</a>";
+    }
+
+    echo "</div></div>";
 }
-echo "</body>";
+echo "</center></body>";
