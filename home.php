95d910d7fa39a45faba0395f6e1c383691c39928
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-02-15 16:53:55 -0500

    grammar

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
 
 
 echo "<head><link rel='shortcut icon' type='image/png' href='style/MosaicLogo.png'/><title>Mosaic</title><meta name='viewport' content='width=device-width, initial-scale=1'>
-<link href='style/style.css' rel='stylesheet'><script src='lib/mosaic.js'></script></head><body onload='getcount();'><a href='authorize.php'>Share your top tracks for the month</a></br><center><h1>Welcome to Mosaic</h1> <span>Check out <span id='count'>0</span> user's top songs for the past 4 weeks. Click on a song to listen to it or share yours by clicking the link in the top left</span></center></br><center>";
+<link href='style/style.css' rel='stylesheet'><script src='lib/mosaic.js'></script></head><body onload='getcount();'><a href='authorize.php'>Share your top tracks for the month</a></br><center><h1>Welcome to Mosaic</h1> <span>Check out <span id='count'>0</span> users' top songs for the past 4 weeks. Click on a song to listen to it or share yours by clicking the link in the top left</span></center></br><center>";
 
 $users = json_decode(file_get_contents('lists.json'),true);
 foreach ($users as $user) {
