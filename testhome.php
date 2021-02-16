0af20e0a5cfff26191275f50b0c5751a4bb69db8
dyvision <dlang@dssinc.com>
2021-02-16 14:32:51 -0500

    there

--- testhome.php
+++ testhome.php
@@ -18,7 +18,6 @@ $usercli = new top();
 $user = json_decode($usercli->get(null,$_COOKIE['username']),true);
 
 echo "<div id='sidebar'><img class='profile' src='" . $user['ava'] . "'></img><div class='songlist'><a href='" . $user['url']. "'><h2>" . $user['display_name'] . "</h2></a>";
-echo json_encode($user);
 foreach ($user['tracks'] as $track) {
     echo "<a href='" . $track['link'] . "'>" . $track['name'] . "</a>";
 }
