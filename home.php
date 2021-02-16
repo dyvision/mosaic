d96332dba61cc9d6124077852777e4833152bc62
dyvision <dlang@dssinc.com>
2021-02-16 15:52:40 -0500

    privacy matters

--- home.php
+++ home.php
@@ -3,12 +3,13 @@
 include('lib/mosaic.php');
 
 use mosaic\top;
+use mosaic\user;
 
 $kofi = "<script type='text/javascript' src='https://ko-fi.com/widgets/widget_2.js'></script><script type='text/javascript'>kofiwidget2.init('Support Mosaic on Ko-fi', '#29abe0', 'E1E822CWN');kofiwidget2.draw();</script>";
 
-if(isset($_COOKIE['refresh'])){
+if (isset($_COOKIE['refresh'])) {
     $header = "<h3 class='navbar-item'><a  onclick='showprofile();'>Profile</a></h3><h3 class='navbar-item'><a  href='api/logout.php'>Logout</a></h3>$kofi";
-}else{
+} else {
     $header = "<h3 class='navbar-item'><a href='authorize.php'>Connect</a></h3>$kofi";
 }
 
@@ -17,9 +18,12 @@ echo "<head><link rel='shortcut icon' type='image/png' href='style/MosaicLogo.pn
 <link href='style/style.css' rel='stylesheet'><script src='lib/mosaic.js'></script></head><body onload='getcount();'></br><center><h1>Welcome to Mosaic</h1>$header</br><span>Check out <span id='count'>0</span> users' top songs for the past 4 weeks. Click on a song to listen to it or share yours by clicking connect</span></center></br>";
 
 $usercli = new top();
-$user = json_decode($usercli->get(null,$_COOKIE['username']),true);
+$lookupcli = new user();
 
-echo "<div id='sidebar'><img class='profile' style='width:100%;' src='" . $user['ava'] . "'></img><div class='songlist'><a href='" . $user['url']. "'><h2>" . $user['display_name'] . "</h2></a>";
+
+$user = json_decode($usercli->get(null, $_COOKIE['username']), true);
+
+echo "<div id='sidebar'><img class='profile' style='width:100%;' src='" . $user['ava'] . "'></img><div class='songlist'><a href='" . $user['url'] . "'><h2>" . $user['display_name'] . "</h2></a>";
 foreach ($user['tracks'] as $track) {
     echo "<a href='" . $track['link'] . "'>" . $track['name'] . "</a>";
 }
@@ -27,16 +31,20 @@ echo "<h3><a style='display:none'>Groups</a></h3><h3><a onclick='hideprofile();'
 
 
 
-$users = json_decode(file_get_contents('lists.json'),true);
-foreach ($users as $user) {
-    if ($user == null) {
-    } else {
-        echo "<div class='block'><img class='profile' src='" . $user['ava'] . "'></img><div class='songlist'><a href='" . $user['url']. "'><h2>" . $user['display_name'] . "</h2></a>";
-        foreach ($user['tracks'] as $track) {
-            echo "<a href='" . $track['link'] . "'>" . $track['name'] . "</a>";
-        }
+$users = json_decode(file_get_contents('lists.json'), true);
 
-        echo "</div></div>";
+foreach ($users as $user) {
+    if (json_decode($lookupcli->get($user['id']), true)['private'] == true) {
+    } else {
+        if ($user == null) {
+        } else {
+            echo "<div class='block'><img class='profile' src='" . $user['ava'] . "'></img><div class='songlist'><a href='" . $user['url'] . "'><h2>" . $user['display_name'] . "</h2></a>";
+            foreach ($user['tracks'] as $track) {
+                echo "<a href='" . $track['link'] . "'>" . $track['name'] . "</a>";
+            }
+
+            echo "</div></div>";
+        }
     }
 }
 echo "</center></body>";
