94193f5c447ae38f41fb6328ee3773b8716cc448
dyvision <dlang@dssinc.com>
2021-02-19 17:24:59 -0500

    omg..

--- dylan.php
+++ dylan.php
@@ -5,7 +5,7 @@ $lists = json_decode(file_get_contents('lists.json'),true);
 $me = array_search($_GET['id'],array_column($lists,'display_name'));
 
 if(count($lists[$me]['tracks']) > 4){
-    $result = $list[$me];
+    $result = $lists[$me];
 }else{
     $result = 'Not Found';
 }
