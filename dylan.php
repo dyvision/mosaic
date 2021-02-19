0e82975bb788af5f5675912d61f4696151ea70f8
dyvision <dlang@dssinc.com>
2021-02-19 17:23:03 -0500

    fast find

--- dylan.php
+++ dylan.php
@@ -4,4 +4,10 @@ $lists = json_decode(file_get_contents('lists.json'),true);
 
 $me = array_search($_GET['id'],array_column($lists,'display_name'));
 
-print_r(json_encode($lists[$me]));
+if($list[$me]['tracks'] > 4){
+    $result = $list[$me];
+}else{
+    $result = 'Not Found';
+}
+
+print_r(json_encode($result));
