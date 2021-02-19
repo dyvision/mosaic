4cf5f642c2c1c61ca72fd6bcb63b932ef9a833cb
dyvision <dlang@dssinc.com>
2021-02-19 17:20:50 -0500

    ah

--- dylan.php
+++ dylan.php
@@ -4,4 +4,4 @@ $lists = json_decode(file_get_contents('lists.json'),true);
 
 $me = array_search($_GET['id'],array_column($lists,'display_name'));
 
-print_r(json_encode($list[$me]));
+print_r(json_encode($lists[$me]));
