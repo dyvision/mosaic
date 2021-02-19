ecab163354b0dbb084e6fb5c2ab826e545313f14
dyvision <dlang@dssinc.com>
2021-02-19 17:19:19 -0500

    test

--- dylan.php
+++ dylan.php
@@ -2,6 +2,6 @@
 
 $lists = json_decode(file_get_contents('lists.json'),true);
 
-$me = array_search($_GET['id'],$lists,true);
+$me = array_search($_GET['id'],array_column($lists,'display_name'));
 
 print_r(json_encode($me));
